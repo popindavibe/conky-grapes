398a2a3c079c7e393bc1cf0bf99d4f84b37c0261
popindavibe <popi+github@nomagic.fr>
2017-06-29 16:59:32 +0100

    minor alignment on license

--- create_config.py
+++ create_config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#     This script creates configuration files for conky and lua based on
-#     your machines's current resources.
-#     Copyright (C) 2017  popi
+#    This script creates configuration files for conky and lua based on
+#    your machines's current resources.
+#    Copyright (C) 2017  popi
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
