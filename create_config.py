2ea4535f139f0eb1dc552b8986bae16c6bfff612
Jean M <jean.michaud@rackspace.co.uk>
2018-01-28 14:00:05 +0000

    copyleft

--- create_config.py
+++ create_config.py
@@ -3,7 +3,6 @@
 
 #    This script creates configuration files for conky and lua based on
 #    your machines's current resources.
-#    Copyright (C) 2017  popi
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
